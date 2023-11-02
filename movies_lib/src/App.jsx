735d1b75e395bbb3f5082675b0628f5ac26e8064
Mizael Oliveira <mizaeloliveira8@gmail.com>
2023-11-02 19:49:54 -0300

    Add complete code

---
+++ movies_lib/src/App.jsx
@@ -0,0 +1,15 @@
+import { Link, Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar';
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Outlet />
+    </div>  
+  );
+}
+
+export default App
